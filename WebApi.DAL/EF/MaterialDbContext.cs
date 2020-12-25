d8878b0ab0ab10dc308db037a2c8c5941d9b5909
Roman <tinkotsu@gmail.com>
2020-12-25 20:05:51 +0500

    added migrations

--- WebApi.DAL/EF/MaterialDbContext.cs
+++ WebApi.DAL/EF/MaterialDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using WebApi.DAL.Entities.Material;
 
 namespace WebApi.DAL.EF
@@ -6,11 +7,20 @@ namespace WebApi.DAL.EF
 
     public class MaterialDbContext : DbContext
     {
+        public MaterialDbContext()
+        { }
 
         public MaterialDbContext(DbContextOptions<MaterialDbContext> options)
             : base(options)
         { }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("server=localhost;port=3306;user=root;password=Ryuzaki12;database=SenatTestApiDb", new MySqlServerVersion(new Version(8, 0, 22)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- WebApi.DAL/EF/UserDbContext.cs
+++ WebApi.DAL/EF/UserDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebApi.DAL.Entities.User;
 
@@ -6,8 +7,19 @@ namespace WebApi.DAL.EF
 {
     public class UserDbContext : IdentityDbContext<ApplicationUser>
     {
+        public UserDbContext()
+        { }
+
         public UserDbContext(DbContextOptions<UserDbContext> options)
             : base(options)
         { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("server=localhost;port=3306;user=root;password=Ryuzaki12;database=SenatTestApiDb", new MySqlServerVersion(new Version(8, 0, 22)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
     }
 }
